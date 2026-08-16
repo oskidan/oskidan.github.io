81a76467b7422e5d33cf7abe88469f1738d5c073
Oleksii Skidan <al.skidan@gmail.com>
2026-08-16 18:22:55 +0200

    refactor(Sandbox): injecting lastTickMillis into Time's constructor

--- Source/Sandbox/Physics/Time.js
+++ Source/Sandbox/Physics/Time.js
@@ -1,9 +1,9 @@
 export class Time {
-  constructor(fps) {
+  constructor(fps, lastTickMillis) {
     this.fps = fps || 30;
     this.dt = 1 / this.fps;
     this.dtSquared = this.dt * this.dt;
-    this.lastTickMillis = performance.now();
+    this.lastTickMillis = lastTickMillis || performance.now();
     this.millisPerTick = 1000 / fps;
     this.elapsedTicks = 0;
   }
@@ -17,17 +17,44 @@ export class Time {
 }
 
 export const testTime = () => {
-    var sut = Time(1)
-    sut.lastTickMillis = 0
-    console.assert(sut.fps == 1, `Expected: sut.fps == 1, got sut.fps == ${sut.fps}`);
-    console.assert(sut.dt == 1, `Expected: sut.dt == 1, got sut.dt == ${sut.dt}`);
-    console.assert(sut.dtSquared == 1, `Expected: sut.dtSquared == 1, got sut.dtSquared == ${sut.dtSquared}`);
-    console.assert(sut.millisPerTick == 1000, `Expected: sut.millisPerTick == 1000, got sut.millisPerTick == ${sut.millisPerTick}`);
-    console.assert(sut.elapsedTicks == 0, `Expected: sut.elapsedTicks == 1000, got sut.elapsedTicks == ${sut.elapsedTicks}`);
-    sut.update(100)
-    console.assert(sut.elapsedTicks == 0, `Expected: sut.elapsedTicks == 0, got sut.elapsedTicks == ${sut.elapsedTicks}`);
-    console.assert(sut.lastTickMillis == 0, `Expected: sut.lastTickMillis == 0, got sut.lastTickMillis == ${sut.lastTickMillis}`);
-    sut.update(1300)
-    console.assert(sut.elapsedTicks == 1, `Expected: sut.elapsedTicks == 1, got sut.elapsedTicks == ${sut.elapsedTicks}`);
-    console.assert(sut.lastTickMillis == 1000, `Expected: sut.lastTickMillis == 1000, got sut.lastTickMillis == ${sut.lastTickMillis}`);
-}
+  var sut = Time(1, 0);
+  console.assert(
+    sut.fps == 1,
+    `Expected: sut.fps == 1, got sut.fps == ${sut.fps}`,
+  );
+  console.assert(sut.dt == 1, `Expected: sut.dt == 1, got sut.dt == ${sut.dt}`);
+  console.assert(
+    sut.dtSquared == 1,
+    `Expected: sut.dtSquared == 1, got sut.dtSquared == ${sut.dtSquared}`,
+  );
+  console.assert(
+    sut.lastTickMillis == 0,
+    `Expected: sut.lastTickMillis == 0, got sut.lastTickMillis == ${sut.lastTickMillis}`,
+  );
+  console.assert(
+    sut.millisPerTick == 1000,
+    `Expected: sut.millisPerTick == 1000, got sut.millisPerTick == ${sut.millisPerTick}`,
+  );
+  console.assert(
+    sut.elapsedTicks == 0,
+    `Expected: sut.elapsedTicks == 1000, got sut.elapsedTicks == ${sut.elapsedTicks}`,
+  );
+  sut.update(100);
+  console.assert(
+    sut.elapsedTicks == 0,
+    `Expected: sut.elapsedTicks == 0, got sut.elapsedTicks == ${sut.elapsedTicks}`,
+  );
+  console.assert(
+    sut.lastTickMillis == 0,
+    `Expected: sut.lastTickMillis == 0, got sut.lastTickMillis == ${sut.lastTickMillis}`,
+  );
+  sut.update(1300);
+  console.assert(
+    sut.elapsedTicks == 1,
+    `Expected: sut.elapsedTicks == 1, got sut.elapsedTicks == ${sut.elapsedTicks}`,
+  );
+  console.assert(
+    sut.lastTickMillis == 1000,
+    `Expected: sut.lastTickMillis == 1000, got sut.lastTickMillis == ${sut.lastTickMillis}`,
+  );
+};
