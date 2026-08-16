bbbf547c78d424044ed63da92d1ea47edd71238e
Oleksii Skidan <al.skidan@gmail.com>
2026-08-16 19:20:44 +0200

    fix(Sandbox): forgot to import Time and Particle

--- Source/Sandbox/main.js
+++ Source/Sandbox/main.js
@@ -1,6 +1,6 @@
 import { testVector2D } from "./Math/Vector2D.js";
-import { testTime } from "./Physics/Time.js";
-import { testParticle } from "./Physics/Particle.js";
+import { testTime, Time } from "./Physics/Time.js";
+import { testParticle, Particle } from "./Physics/Particle.js";
 import { Renderer } from "./Rendering/Renderer.js";
 
 testVector2D();
