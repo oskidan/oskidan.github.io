8f3f56c681c380fb540a148b6c01b1e7f76152f3
Oleksii Skidan <al.skidan@gmail.com>
2026-08-16 18:20:59 +0200

    feat(Sandbox): added Time

---
+++ Source/Sandbox/Physics/Time.js
@@ -0,0 +1,33 @@
+export class Time {
+  constructor(fps) {
+    this.fps = fps || 30;
+    this.dt = 1 / this.fps;
+    this.dtSquared = this.dt * this.dt;
+    this.lastTickMillis = performance.now();
+    this.millisPerTick = 1000 / fps;
+    this.elapsedTicks = 0;
+  }
+  // Updates the game time with a given current timestamp in milliseconds.
+  update(currMillis) {
+    const elapsedMillis = currMillis - this.lastTickMillis;
+    this.elapsedTicks = Math.floor(elapsedMillis / this.millisPerTick);
+    this.lastTickMillis += this.elapsedTicks * this.millisPerTick;
+    return this;
+  }
+}
+
+export const testTime = () => {
+    var sut = Time(1)
+    sut.lastTickMillis = 0
+    console.assert(sut.fps == 1, `Expected: sut.fps == 1, got sut.fps == ${sut.fps}`);
+    console.assert(sut.dt == 1, `Expected: sut.dt == 1, got sut.dt == ${sut.dt}`);
+    console.assert(sut.dtSquared == 1, `Expected: sut.dtSquared == 1, got sut.dtSquared == ${sut.dtSquared}`);
+    console.assert(sut.millisPerTick == 1000, `Expected: sut.millisPerTick == 1000, got sut.millisPerTick == ${sut.millisPerTick}`);
+    console.assert(sut.elapsedTicks == 0, `Expected: sut.elapsedTicks == 1000, got sut.elapsedTicks == ${sut.elapsedTicks}`);
+    sut.update(100)
+    console.assert(sut.elapsedTicks == 0, `Expected: sut.elapsedTicks == 0, got sut.elapsedTicks == ${sut.elapsedTicks}`);
+    console.assert(sut.lastTickMillis == 0, `Expected: sut.lastTickMillis == 0, got sut.lastTickMillis == ${sut.lastTickMillis}`);
+    sut.update(1300)
+    console.assert(sut.elapsedTicks == 1, `Expected: sut.elapsedTicks == 1, got sut.elapsedTicks == ${sut.elapsedTicks}`);
+    console.assert(sut.lastTickMillis == 1000, `Expected: sut.lastTickMillis == 1000, got sut.lastTickMillis == ${sut.lastTickMillis}`);
+}
--- Source/Sandbox/main.js
+++ Source/Sandbox/main.js
@@ -1,6 +1,8 @@
 import { testVector2D } from "./Math/Vector2D.js";
-import { Renderer } from "./Rendering/Renderer.js"
+import { testTime } from "./Physics/Time.js";
+import { Renderer } from "./Rendering/Renderer.js";
 
 testVector2D();
+testTime();
 
-const renderer = new Renderer("#canvas")
+const renderer = new Renderer("#canvas");
