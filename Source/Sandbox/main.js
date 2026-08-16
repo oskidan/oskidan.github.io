8ee1b77e7a84cdd07eb9d19b10626ea96b511d8a
Oleksii Skidan <al.skidan@gmail.com>
2026-08-16 22:27:00 +0200

    feat(Sandbox): does the order matter?

--- Source/Sandbox/main.js
+++ Source/Sandbox/main.js
@@ -28,13 +28,15 @@ const springs = [
 ];
 
 const gameUpdate = () => {
-  for (const spring of springs) {
-    spring.update();
-  }
   for (const particle of particles) {
     // Gravity. Please note that it cannot be modelled as a force,
     // because all objects accelerate due to gravity the same amount.
     particle.applyAccel(0, 3);
+  }
+  for (const spring of springs) {
+    spring.update();
+  }
+  for (const particle of particles) {
     particle.update(time);
   }
 };
